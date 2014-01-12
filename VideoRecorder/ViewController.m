d192c26acfffb95ed89422247ec00bc63720160e
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-01-12 11:15:25 -0500

    added video recording - saves to camera roll and Documents Folder (as video.mov) implemented iTunes File Sharing

--- VideoRecorder/ViewController.m
+++ VideoRecorder/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <MobileCoreServices/UTCoreTypes.h>
 
 @interface ViewController ()
+
+- (IBAction)recordButton:(id)sender;
+- (IBAction)playbackButton:(id)sender;
 
 @end
 
@@ -24,6 +28,73 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)recordButton:(id)sender {
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        picker.delegate = self;
+        picker.allowsEditing = NO;
+        
+        NSArray *mediaTypes = [[NSArray alloc]initWithObjects:(NSString *)kUTTypeMovie, nil];
+        
+        picker.mediaTypes = mediaTypes;
+        
+        [self presentViewController:picker animated:YES completion:nil];
+        
+    } else {
+        
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:nil message:@"I'm afraid there's no camera on this device!" delegate:nil cancelButtonTitle:@"Dang!" otherButtonTitles:nil, nil];
+        [alertView show];
+    }
+}
+
+- (IBAction)playbackButton:(id)sender {
+    
+    // pick a video from the documents directory
+    NSURL *video = [self grabFileURL:@"video.mov"];
+    
+}
+
+
+#pragma mark - Delegate Methods
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    // user hit cancel
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    // grab our movie URL
+    NSURL *chosenMovie = [info objectForKey:UIImagePickerControllerMediaURL];
+    
+    // save it to the documents directory
+    NSURL *fileURL = [self grabFileURL:@"video.mov"];
+    NSData *movieData = [NSData dataWithContentsOfURL:chosenMovie];
+    [movieData writeToURL:fileURL atomically:YES];
+    
+    // save it to the Camera Roll
+    UISaveVideoAtPathToSavedPhotosAlbum([chosenMovie path], nil, nil, nil);
+    
+    // and dismiss the picker
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (NSURL*)grabFileURL:(NSString *)fileName {
+    
+    // find Documents directory
+    NSURL *documentsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    
+    // append a file name to it
+    documentsURL = [documentsURL URLByAppendingPathComponent:fileName];
+
+    return documentsURL;
 }
 
 @end
